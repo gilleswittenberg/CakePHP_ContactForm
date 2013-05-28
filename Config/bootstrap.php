fc909a0f7c4a427e508ce33fae4446f7b84f490f
Gilles Wittenberg <git@gilleswittenberg.com>
2013-05-28 09:59:27 +0200

    Set default configuration in bootstrap

--- Config/bootstrap.php
+++ Config/bootstrap.php
@@ -1,9 +1,10 @@
 <?php
-/*
 Configure::write('ContactForm.mailTo', 'mail@example.com');
 Configure::write('ContactForm.messageSuccess', __('Message send'));
 Configure::write('ContactForm.messageFail', __('Message not send'));
 Configure::write('ContactForm.sendInControllerAction', true);
+Configure::write('ContactForm.ajax', 'true');
+Configure::write('ContactForm.includeJQuery', 'true');
 Configure::write('ContactForm.template.view', 'default');
 Configure::write('ContactForm.template.layout', 'default');
 Configure::write('ContactForm.fields', array(
@@ -14,4 +15,3 @@ Configure::write('ContactForm.fields', array(
 		'type' => 'textarea'
 	)
 ));
-*/
